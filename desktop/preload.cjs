b5b1eb24a10b5bff75c41496a7ac08e1d8087a42
nutricionistaalmeidavh-spec <nutricionistaalmeidavh@gmail.com>
2026-09-09 20:33:54 -0300

    feat: expose narrow E18 hardware preload API

--- desktop/preload.cjs
+++ desktop/preload.cjs
@@ -5,6 +5,13 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('artisysDesktop', {
   getConfig: () => ipcRenderer.invoke('artisys:config'),
   apiRequest: (request) => ipcRenderer.invoke('artisys:api', request),
+  hardware: {
+    status: () => ipcRenderer.invoke('artisys:hardware:status'),
+    readWeight: () => ipcRenderer.invoke('artisys:hardware:scale-read'),
+    tare: () => ipcRenderer.invoke('artisys:hardware:scale-tare'),
+    openDrawer: () => ipcRenderer.invoke('artisys:hardware:drawer-open'),
+    print: (job) => ipcRenderer.invoke('artisys:hardware:print', job)
+  },
   window: {
     minimize: () => ipcRenderer.send('artisys:window:minimize'),
     maximize: () => ipcRenderer.send('artisys:window:maximize'),
