f14e4beabd5010ce91e75a76544bcb8411371541
nutricionistaalmeidavh-spec <nutricionistaalmeidavh@gmail.com>
2026-09-22 15:07:11 -0300

    legacy: isolate legacy installer from modern update channel

--- desktop/updater-main.cjs
+++ desktop/updater-main.cjs
@@ -5,10 +5,14 @@ const { autoUpdater } = require('electron-updater');
 const { createUpdaterService } = require('./updater-service.cjs');
 require('./main.cjs');
 
+const electronMajor = Number(String(process.versions.electron || '').split('.')[0] || 0);
+const legacyRuntime = electronMajor > 0 && electronMajor <= 22;
+
 const service = createUpdaterService({
   app,
   autoUpdater,
   updateUrl: process.env.ARTISYS_UPDATE_URL || '',
+  enabled: !legacyRuntime,
   onState: (state) => {
     for (const window of BrowserWindow.getAllWindows()) {
       window.webContents.send('updater:state', state);
