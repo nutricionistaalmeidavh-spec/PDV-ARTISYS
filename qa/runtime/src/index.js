32e64413dfa926f49994ff13bfc13eec50a4cc61
nutricionistaalmeidavh-spec <nutricionistaalmeidavh@gmail.com>
2026-09-25 23:07:13 -0300

    feat: export crosscut contract runner

--- qa/runtime/src/index.js
+++ qa/runtime/src/index.js
@@ -73,4 +73,5 @@ export { createQaRemoteControl } from './remote-control.js';
 export { buildQaMatrix, runQaMatrix } from './matrix.js';
 export { runApiSweep } from './api-sweep.js';
 export { runUiSweep } from './ui-sweep.js';
+export { runCrosscutContracts } from './crosscut-runner.js';
 export { evaluateProductGate, buildProductQaSummary, renderProductQaText, renderProductQaHtml, writeProductQaBundle } from './product-report.js';
